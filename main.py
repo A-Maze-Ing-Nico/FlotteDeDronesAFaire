28a0a2ee7dc1e3e1f30468ef1a99b790718ab6a4
Nico <2472096@cegepoutaouais.qc.ca>
2026-08-28 09:12:46 -0400

    Changement d'une variable protégée

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 if __name__ == "__main__":
     from drones import DroneLeger
-    from drones import Drone
 
     dl: DroneLeger = DroneLeger(identifiant="23rT", latitude=0, longitude=0)
-    print(dl._batterie)
+    print(dl.batterie)
     dl.consommer_batterie()
     print(dl.batterie)
